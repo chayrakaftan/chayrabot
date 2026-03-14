8436fb8caad1911589407bc5aea8b7f48b3bff53
chayrakaftan <chayrakaftan@gmail.com>
2026-03-14 01:23:40 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'ChayraBot',
+  description: 'Chatbot ChayraKaftan',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
